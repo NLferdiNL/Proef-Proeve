3ece828eaec93b4571a675c227f1a38bbfbf1544
Ferdi <nlferdinl@gmail.com>
2018-03-06 23:35:28 +0100

    Basic debug movement

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Circle : MonoBehaviour {
+
+	static int _rotationAngle = 45;
+
+	Image _image;
+
+	RectTransform _transform;
+
+	bool _rotating = false;
+
+	[SerializeField]
+	float _rotationSpeed = 1;
+
+	public Color color {
+		get {
+			if(_image == null)
+				return Color.white;
+
+			return _image.color;
+		}
+
+		set {
+			if(_image == null)
+				return;
+
+			_image.color = value;
+		}
+	}
+
+	private void Start() {
+		_image = GetComponent<Image>();
+		_transform = GetComponent<RectTransform>();
+	}
+
+	public void Generate() {
+		// Place images
+	}
+
+	public void MoveLeft() {
+		StartCoroutine(Move(-_rotationAngle));
+	}
+
+	public void MoveRight() {
+		StartCoroutine(Move(_rotationAngle));
+	}
+
+	IEnumerator Move(float addedAngle) {
+		if(!_rotating) {
+			_rotating = true;
+
+			Quaternion goalAngle = _transform.rotation * Quaternion.Euler(0, 0, addedAngle);
+			Quaternion startAngle = _transform.rotation;
+
+			float position = 0;
+
+			while(position < 1) {
+				position += Time.deltaTime * _rotationSpeed;
+				_transform.rotation = Quaternion.Lerp(startAngle, goalAngle, position);
+				yield return new WaitForEndOfFrame();
+			}
+
+			_transform.rotation = goalAngle;
+			_rotating = false;
+		}
+	}
+}
---
+++ Assets/Scripts/DebugMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugMovement : MonoBehaviour {
+
+	int selectedCircleIndex = 0;
+
+	[SerializeField]
+	Circle[] circles = new Circle[3];
+
+	Circle selectedCircle {
+		get {
+			return circles[selectedCircleIndex];
+		}
+	}
+
+	[SerializeField]
+	Color selectedColor, regularColor;
+
+	private void Start() {
+		selectedCircle.color = selectedColor;
+	}
+
+	private void FixedUpdate() {
+		bool left, right, up, down = false;
+
+		left = Input.GetKeyDown(KeyCode.A);
+		right = Input.GetKeyDown(KeyCode.D);
+		up = Input.GetKeyDown(KeyCode.W);
+		down = Input.GetKeyDown(KeyCode.S);
+
+		if (left && right)
+			left = right = false;
+
+		if (up && down)
+			up = down = false;
+
+		if(left)
+			MoveRight();
+
+		if(right)
+			MoveLeft();
+
+		if(up || down)
+			ResetColorFromCircle();
+
+		if(up)
+			selectedCircleIndex--;
+
+		if(down)
+			selectedCircleIndex++;
+
+		if(selectedCircleIndex <= -1)
+			selectedCircleIndex = circles.Length - 1;
+		else if(selectedCircleIndex >= circles.Length)
+			selectedCircleIndex = 0;
+
+		SetColorToCircle();
+	}
+
+	void MoveLeft() {
+		selectedCircle.MoveLeft();
+	}
+
+	void MoveRight() {
+		selectedCircle.MoveRight();
+	}
+
+	void ResetColorFromCircle() {
+		selectedCircle.color = regularColor;
+	}
+
+	void SetColorToCircle() {
+		selectedCircle.color = selectedColor;
+	}
+}
