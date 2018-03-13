a88d42a1e12f5579c18ec350f4ab8eaace4cbe79
The1Black\swan <swancd@gmail.com>
2018-03-13 23:19:12 +0100

    working on Sound
    making a sound manager

--- Assets/Scripts/Audio/SoundAndMusic.cs
+++ Assets/Scripts/Audio/SoundAndMusic.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MuteSound : MonoBehaviour {
+public class SoundAndMusic : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
