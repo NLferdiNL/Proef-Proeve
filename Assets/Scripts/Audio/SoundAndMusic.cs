9d0de2bfa3fed09c55f15ea978bc43d245ad5b36
The1Black\swan <swancd@gmail.com>
2018-03-14 10:16:58 +0100

    Options menu
    added some art
    re-did some buttons and sliders

--- Assets/Scripts/Audio/SoundAndMusic.cs
+++ Assets/Scripts/Audio/SoundAndMusic.cs
@@ -2,15 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SoundAndMusic : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+public class SoundAndMusic : MonoBehaviour
+{
+    public static SoundAndMusic instance = null;     //Allows other scripts to call functions from SoundManager.             
+    
 }
