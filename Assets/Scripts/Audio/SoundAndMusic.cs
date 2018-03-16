bad41c1d6dfcfc14c9737ccc3469ad7d85734449
Dylan Waij <d_way@live.nl>
2018-03-16 11:43:40 +0100

    Push

--- Assets/Scripts/Audio/SoundAndMusic.cs
+++ Assets/Scripts/Audio/SoundAndMusic.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class SoundAndMusic : MonoBehaviour
 {
     public static SoundAndMusic instance = null;     //Allows other scripts to call functions from SoundManager.             
+    //public AudioClip[] AudioLibrary;
     public AudioClip[] Music;
     public AudioClip[] SoundEffects;
 
     void Awake()
     {
         Music = Resources.LoadAll<AudioClip>("Audio/Music");
-        foreach (AudioClip clip in Music)
+        SoundEffects = Resources.LoadAll<AudioClip>("Audio/SoundEffects");
+
+
+
+        /*
+        foreach (AudioClip clip in AudioLibrary)
         {
             
-        }
+        }*/
+
     }
+
 
 }
