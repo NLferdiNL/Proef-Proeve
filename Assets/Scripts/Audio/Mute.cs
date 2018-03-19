31b596bcb685d52b7d6eb9cc1c9acb29e6d0caa5
Dylan Waij <d_way@live.nl>
2018-03-19 15:00:46 +0100

    Music and Sfx.
    Slider, audioloader with play and near empty mute script if needed.

---
+++ Assets/Scripts/Audio/Mute.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mute : MonoBehaviour {
+
+    private bool Muted;
+
+    public void Check()
+    {
+        if (!Muted)
+        {
+            
+            Muted = true;
+            return;
+        }
+        else if (Muted)
+        {
+
+            Muted = false;
+            return;
+        }
+    }
+}
---
+++ Assets/Scripts/Audio/VolumeSlider.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VolumeSlider : MonoBehaviour {
+
+    public Slider slider;
+
+    public void SlideAudio()
+    {
+        AudioListener.volume = slider.value;
+    }
+}
