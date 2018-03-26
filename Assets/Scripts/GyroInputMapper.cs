3f13aeff5d3aac18a6f5c6ddf538282316eb1489
The1Black\swan <swancd@gmail.com>
2018-03-26 03:19:30 +0200

    Gyro/parralaxBG
    attampt at using gyro to make a moving background but cant test

---
+++ Assets/Scripts/GyroInputMapper.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GyroInputMapper : MonoBehaviour
+{
+    private Vector3 _gyroRotationRate;
+    private bool _gyroState = false;
+
+    private static GyroInputMapper instance;
+
+    void Start()
+    {
+        instance = this;
+
+        if (Input.gyro.enabled)
+        {
+            _gyroState = true;
+            Debug.Log("yay it works");
+        }
+        else
+        {
+            Debug.Log("This device may not have a Gyro");
+            _gyroState = false;
+            Input.gyro.enabled = true;
+        }
+
+    }
+
+    public static bool gyroState
+    {
+        get
+        {
+            return instance._gyroState;
+        }
+    }
+
+    public static Vector3 GyroRotationRate
+    {
+        get
+        {
+            return instance._gyroRotationRate;
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        _gyroRotationRate = Input.gyro.rotationRate;
+    }
+}
---
+++ Assets/Scripts/ParallaxObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParallaxObject : MonoBehaviour
+{
+
+    [SerializeField]
+    float depth = -1;
+
+    [SerializeField]
+    bool useZ = true;
+
+    Vector3 startPosition;
+
+    private void Start()
+    {
+        if (useZ)
+            depth = transform.position.z;
+
+        startPosition = transform.position;
+
+        if (GyroInputMapper.gyroState)
+            Debug.Log("Yay there is a gyro");
+        else
+            Debug.Log(":( no gyro -_-");
+    }
+
+    private void FixedUpdate()
+    {
+        if (GyroInputMapper.gyroState)
+        {
+            transform.position = startPosition + transform.right * GyroInputMapper.GyroRotationRate.x * depth + transform.up * GyroInputMapper.GyroRotationRate.y * depth;
+        }
+        else
+        {
+            transform.position = startPosition + transform.right * ParallaxManager.XRot * depth + transform.up * ParallaxManager.YRot * depth;
+        }
+    }
+}
