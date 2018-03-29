eddbb0b6b1cd2281d9e46b7550f154a2cb39945d
Dylan Waij <d_way@live.nl>
2018-03-29 10:36:15 +0200

    Udate sound

--- Assets/Scripts/Audio/VolumeSlider.cs
+++ Assets/Scripts/Audio/VolumeSlider.cs
@@ -1,14 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class VolumeSlider : MonoBehaviour {
+public class VolumeSlider : MonoBehaviour
+{
 
     public Slider slider;
+
+    private static float holdvolume;
+
+    public static float HoldVolume
+    {
+        get
+        {
+            return holdvolume;
+        } set {
+            holdvolume = value;
+        }
+    }
 
     public void SlideAudio()
     {
         AudioListener.volume = slider.value;
+        slider.value = HoldVolume;
     }
+
 }
